c983f0d3f880177d4d82f0940b6cb2dcfcdd89a8
Craig Davis <cdavis@stoneydavis.com>
2021-03-06 17:32:24 -0600

    Update to log nginx and wokd logs to stdout

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,10 @@ set -x
 nginx -V
 nginx &
 test -d /run/user/0 || mkdir -p /run/user/0
+test -d /var/log/wok || mkdir -p /var/log/wok
+test -e /var/log/wok/wok-error.log || touch /var/log/wok/wok-error.log
+test -e /var/log/wok/wok-access.log || touch /var/log/wok/wok-access.log
+
+tail -f /var/log/wok/wok-error.log /var/log/wok/wok-access.log /var/log/nginx/access.log /var/log/nginx/error.log &
+
 /usr/bin/wokd
